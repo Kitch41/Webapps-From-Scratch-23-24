2586cab293870e5ea00e09c7a2e63986b7af6f97
Kitch41 <stefkeuken@hotmail.com>
2024-02-06 16:38:53 +0100

    day 2 fixes

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -23,10 +23,56 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     };
 
-    
-
     sidebarbutton.addEventListener("click", togglemenu);
 
+    const shopbutton = document.getElementsByClassName("shopbutton")[0];
+    const shopbuttonsvg = document.getElementsByClassName("shop-svg")[0];
+    const shopsection = document.getElementsByClassName("shopsection")[0];
+    var shopopen = 0;
+    
+    function toggleshop() {
+        if (shopopen === 0) {
+            console.log("shop open");
+            shopbuttonsvg.style.transform = 'rotate(270deg)';
+            shopsection.style.bottom = '0px';
+            shopbutton.style.bottom = '11em';
+            
+            shopopen = 1;
+        } else if (shopopen === 1) {
+            console.log("shop close");
+            shopbuttonsvg.style.transform = 'rotate(90deg)';
+            shopsection.style.bottom = '-200px';
+            shopbutton.style.bottom = '1em';
+
+            shopopen = 0;
+        }
+    }
+    
+    shopbutton.addEventListener("click", toggleshop);
+
+
+
+
+    const name = document.getElementById("name");
+    const description = document.getElementById("description");
+    const hobbys = document.getElementById("hobbys");
+    const animals = document.getElementById("animals");
+
+
+
+    async function fetchdata() {
+
+        const response = await fetch("../docs/info.json");
+        const data = await response.json();
+    
+        name.innerHTML = data.name;
+        description.innerHTML = data.description;
+        hobbys.innerHTML = data.hobbys[0].hobby1;
+        animals.innerHTML = data.favoritePets;
+
+    }
+    
+    fetchdata();
 
 
   });
